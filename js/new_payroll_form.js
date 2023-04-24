457370143e7b3e864fbed5eb0a6a8b4c50d21b41
prajwalgunjal <prajwalgunjal8@gmail.com>
2023-04-24 16:55:31 +0530

    uc10 added

--- js/new_payroll_form.js
+++ js/new_payroll_form.js
@@ -57,7 +57,26 @@ class EmployeeParollData{
         return this._startDate;
     }
     set startDate(startDate){
-        this._startDate=startDate;
+          // Check if the input is a valid date
+    const inputDate = new Date(startDate);
+    if (isNaN(inputDate.getTime())) {
+        throw new Error("Invalid date format. Please use yyyy-mm-dd format.");
+    }
+        // Check if the date is not in the future
+    const currentDate = new Date();
+    if (inputDate > currentDate) {
+        throw new Error("Start date cannot be in the future.");
+    }
+
+    // Check if the date is within 30 days of joining
+    const thirtyDaysAgo = new Date();
+    thirtyDaysAgo.setDate(currentDate.getDate() - 30);
+    if (inputDate < this._joinDate || inputDate > currentDate) {
+        throw new Error("Start date should be within 30 days of joining.");
+    }
+
+    // Set the start date if all validation checks pass
+    this._startDate = inputDate;
     }
 toString(){
     const options ={year :'numeric',month:'long',day:'numeric'}
